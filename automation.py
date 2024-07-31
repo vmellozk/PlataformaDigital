b92bb664b432607ecebff13f37f941bdbc9ca9d3
Victor <victorhugoarx@gmail.com>
2024-07-31 13:16:04 -0300

    adicionando um await conforme os botões

--- automation.py
+++ automation.py
@@ -2,6 +2,8 @@ import time
 import os
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import undetected_chromedriver as uc
 import pyperclip
 import pyautogui
@@ -15,13 +17,15 @@ def chatgpt_response(responses_file, output_directory, name):
         os.makedirs(output_directory)
 
     try:
+        # Abre o navegador e define o tempo máximo de espera
         driver.get('https://chat.openai.com')
-        time.sleep(3)
+        wait = WebDriverWait(driver, 5)
 
-        input_field = driver.find_element(By.XPATH, '//*[@id="prompt-textarea"]')
-        time.sleep(0.5)
+        # Espera até que o campo de entrada esteja presente e clicável
+        input_field = wait.until(
+        EC.element_to_be_clickable((By.XPATH, '//*[@id="prompt-textarea"]'))
+    )
         input_field.click()
-        time.sleep(0.5)
 
         # Lê o texto do arquivo de respostas
         with open(responses_file, 'r', encoding='utf-8') as file:
@@ -30,9 +34,9 @@ def chatgpt_response(responses_file, output_directory, name):
         # Criei uma lista das funções dos prompts para não ter que repetir o for em cada um prompt
         prompts = [
             (get_initial_prompt(), None),
-            (responses(responses_text), None),
-            (get_cover_prompt(name), 'capa.txt', 7),
-            (get_table_of_contents_prompt(), 'sumario.txt', 9),
+            (responses(responses_text), None, 17),
+            (get_cover_prompt(name), 'capa.txt', 5),
+            (get_table_of_contents_prompt(), 'sumario.txt', 7),
             (get_intro_prompt(), 'introducao.txt', 20),
             (get_content_prompt(), 'conteudoprincipal.txt', 120),
             (get_conclusion_prompt(), 'conclusao.txt', 40)
@@ -47,20 +51,20 @@ def chatgpt_response(responses_file, output_directory, name):
             if wait_time:
                 time.sleep(wait_time[0])
             else:
-                time.sleep(5)
+                time.sleep(3)
 
             # Verifica se o filename é None antes de tentar copiar e salvar o texto
             if filename:
                 try:
-                    print(f"Tentando localizar o botão de copiar para {filename}.")
-                    pyautogui.click(pyautogui.locateCenterOnScreen('static/images/button_copy_gpt.png'))
-                    time.sleep(1)
+                    copy_field = wait.until(
+                    EC.element_to_be_clickable((By.XPATH, '//*[@id="__next"]'))
+                )
+                    copy_field.click()
                     copied_text = pyperclip.paste()
 
                     # Salva a resposta em um arquivo separado
                     with open(f"{output_directory}/{filename}", "w", encoding="utf-8") as file:
                         file.write(copied_text)
-                    print(f"Resposta salva em: {output_directory}/{filename}")
 
                 except Exception as e:
                     print(f"Erro durante a automação para {filename}: {e}")
