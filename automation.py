791293dee53b2389b195d62fca9b5689d3a567e4
Victor <victorhugoarx@gmail.com>
2024-08-08 08:59:17 -0300

    adicionando um tratamento de erro para quando pedir para logar no chatgpt

--- automation.py
+++ automation.py
@@ -5,8 +5,26 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import undetected_chromedriver as uc
 from prompt import get_initial_prompt, responses, tittle
+from error_handling import click_image_if_found 
+import threading
 
+# Declara a variável global para 
+image_check_thread = None
+
+# Verifica continuamente se a imagem está presente e clica na imagem especificada.
+def continuously_check_images():
+    while True:
+        try:
+            found = click_image_if_found('static/error/thanks_for_use_error.png', 'static/error/continue_desconected.png')
+            if found:
+                pass
+        except Exception as e:
+            pass
+        time.sleep(2)
+
+# Declara que vamos usar a variável global e 
 def chatgpt_response(responses_file, output_file, tittle_file, name):
+    global image_check_thread
     driver = uc.Chrome(version_main=126)
 
     try:
@@ -14,6 +32,10 @@ def chatgpt_response(responses_file, output_file, tittle_file, name):
         driver.maximize_window()
         driver.get('https://chat.openai.com')
         time.sleep(2)
+
+        # Inicia a verificação contínua em uma thread separada
+        image_check_thread = threading.Thread(target=continuously_check_images, daemon=True)
+        image_check_thread.start()
 
         #
         input_field = driver.find_element(By.XPATH, '//*[@id="prompt-textarea"]')
@@ -49,7 +71,7 @@ def chatgpt_response(responses_file, output_file, tittle_file, name):
         input_field.send_keys(Keys.ENTER)
         time.sleep(8)
 
-        #Aguarda a resposta ser gerada e o botão de copiar estar disponível
+        # Aguarda a resposta ser gerada e o botão de copiar estar disponível
         while True:
             try:
                 copy_button_location = pyautogui.locateCenterOnScreen('static/images/button_copy_chat_gpt.png')
@@ -64,18 +86,20 @@ def chatgpt_response(responses_file, output_file, tittle_file, name):
                 print(f"Erro durante a automação: {e}")
                 time.sleep(1)
 
+        #
         copied_tittle = pyperclip.paste()
         with open(tittle_file, "w", encoding="utf-8") as file:
             file.write(copied_tittle)
         time.sleep(1)
     
+        #
         confirmacao = 'OK, agora me forneça o restante do conteúdo. Lembrando da hash antes de: ####Introdução, ####Sumário, ####Conteúdo e ####Conclusão. Forneça esses tópicos assim e tudo em um único texto! Apenas responda o que foi pedido, sem "essa foi a resposta, se precisar de mais..." não quero nada disso. '
         input_field.send_keys(confirmacao)
         time.sleep(1)
         input_field.send_keys(Keys.ENTER)
         time.sleep(45)
 
-        #Aguarda a resposta ser gerada e o botão de copiar estar disponível
+        # Aguarda a resposta ser gerada e o botão de copiar estar disponível
         while True:
             try:
                 copy_button_location = pyautogui.locateCenterOnScreen('static/images/button_copy_chat_gpt.png')
@@ -90,12 +114,16 @@ def chatgpt_response(responses_file, output_file, tittle_file, name):
                 print(f"Erro durante a automação: {e}")
                 time.sleep(1)
 
+        #
         copied_text = pyperclip.paste()
         with open(output_file, "w", encoding="utf-8") as file:
             file.write(copied_text)
             time.sleep(1)
 
     finally:
+        # Encerrar a verificação contínua
+        if image_check_thread is not None:
+            image_check_thread.join(timeout=10)
         try:
             driver.quit()
         except Exception as e:
