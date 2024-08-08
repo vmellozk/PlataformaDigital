c3bb2a0f0adcaa4c3aeb0a45c567bda7ac70ed91
Victor <victorhugoarx@gmail.com>
2024-08-08 11:21:02 -0300

    renomeação de text_utils

---
+++ clear_caracters.py
@@ -0,0 +1,12 @@
+import re
+
+def clean_text(text):
+    # Definindo palavras específicas para remover e a expressão regular para caracteres especiais que devem ser removidos, excluindo os que queremos manter
+    words_to_remove = ['Capa', 'Título: ', 'Autor: ']
+    allowed_characters = r'[^A-Za-z0-9\s.,?!:;()+=/<>-áàâãéèêíìîóòôõúùûç]'
+
+    # Remove palavras específicas e remove caracteres especiais não permitidos
+    for word in words_to_remove:
+        text = re.sub(r'\b' + re.escape(word) + r'\b', '', text, flags=re.IGNORECASE)
+    text = re.sub(allowed_characters, '', text)
+    return text
