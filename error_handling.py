e23aacdc377643a04cf2f40fc502f312086fb569
Victor <victorhugoarx@gmail.com>
2024-08-08 11:18:50 -0300

    tratamento de erro se tiver problema com internet

--- error_handling.py
+++ error_handling.py
@@ -1,8 +1,20 @@
 import time
 import pyautogui
 import os
-import sys
-import time
+
+# Apaga arquivos específicos
+def delete_files(file_paths):
+    for file_path in file_paths:
+        if os.path.exists(file_path):
+            os.remove(file_path)
+
+# Atualiza a página e reinicia o script
+def handle_error(driver, input_field, respons_file, tittle_file, name):
+    if pyautogui.locateCenterOnScreen('static/error/network_error.png', confidence=0.7):
+        time.sleep(1)
+        delete_files(['output.txt', 'tittle.txt'])
+        pyautogui.hotkey('f5')
+        time.sleep(5)
 
 # Verifica se a imagem 'image_path' está na tela e clica na imagem 'click_image_path' se encontrada
 def click_image_if_found(image_path, click_image_path):
@@ -15,6 +27,7 @@ def click_image_if_found(image_path, click_image_path):
                 pyautogui.click(click_location)
                 time.sleep(1)
                 return True
+        return False
     except pyautogui.ImageNotFoundException:
         # adicionar um logging
         pass
