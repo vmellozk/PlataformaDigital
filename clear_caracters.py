26789715ed16f221562ce08d3565d01802d96893
Victor <victorhugoarx@gmail.com>
2024-08-09 11:34:08 -0300

    adicionando mais um filtro para o arquivo txt

--- clear_caracters.py
+++ clear_caracters.py
@@ -2,7 +2,7 @@ import re
 
 def clean_text(text):
     # Definindo palavras específicas para remover e a expressão regular para caracteres especiais que devem ser removidos, excluindo os que queremos manter
-    words_to_remove = ['Capa', 'Título: ', 'Autor: ']
+    words_to_remove = ['Capa', 'Título: ', 'Autor: ', '**Título:**', '**Autor:**']
     allowed_characters = r'[^A-Za-z0-9\s.,?!:;()+=/<>-áàâãéèêíìîóòôõúùûç]'
 
     # Remove palavras específicas e remove caracteres especiais não permitidos
