9880b7452ae634a75c6f4c79b6b324b59f672221
Victor <victorhugoarx@gmail.com>
2024-08-15 12:36:01 -0300

    atualização dos arquivos principais e exclusão dos arquivos de teste

--- automation.py
+++ automation.py
@@ -1,128 +1,81 @@
 import time
-import pyautogui
-import pyperclip
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import undetected_chromedriver as uc
-from prompt import get_initial_prompt, responses, tittle
-from handling_error import click_image_if_found 
+from handling_error import click_image_if_found, handle_error
+from send_prompt import send_prompts
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import threading
+import pyautogui
 
-# Declara a variável global
+#
 image_check_thread = None
+error_check_thread = None
 
-# Verifica continuamente se a imagem está presente e clica na imagem especificada.
+#
 def continuously_check_images():
     while True:
         try:
             found = click_image_if_found('static/error/thanks_for_use_error.png', 'static/error/continue_desconected.png')
             if found:
-                print("Imagem encontrada e clique efetuado.")
+                pass
         except Exception as e:
-            # Se desejar, pode adicionar um print aqui para saber quando um erro não esperado acontece.
-            pass  # Não faz nada se ocorrer uma exceção
+            pass
         time.sleep(2)
 
-# Declara que vamos usar a variável global e 
+#
+def continuously_check_errors(driver, responses_file, tittle_file, name):
+    while True:
+        try:
+            handle_error(driver, responses_file, tittle_file, name)
+            time.sleep(10)
+        except Exception as e:
+            pass
+        time.sleep(2)
+
+#
 def chatgpt_response(responses_file, output_file, tittle_file, name):
-    global image_check_thread
+    global image_check_thread, error_check_thread
     driver = uc.Chrome(version_main=126)
 
     try:
-        #
         driver.maximize_window()
         driver.get('https://chat.openai.com')
         time.sleep(2)
 
-        # Inicia a verificação contínua em uma thread separada
-        image_check_thread = threading.Thread(target=continuously_check_images, daemon=True)
-        image_check_thread.start()
+        while not pyautogui.locateCenterOnScreen('static/images/chatgpt.png'):
+            print("Aguardando a imagem 'chatgpt.png' antes de continuar...")
+            time.sleep(2)
 
-        #
-        input_field = driver.find_element(By.XPATH, '//*[@id="prompt-textarea"]')
+        input_field = WebDriverWait(driver, 15).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="prompt-textarea"]'))
+        )
         time.sleep(1)
         input_field.click()
         time.sleep(1)
 
-        # Lê o texto do arquivo de respostas
-        with open(responses_file, 'r', encoding='utf-8') as file:
-            responses_text = file.read()
+        image_check_thread = threading.Thread(target=continuously_check_images, daemon=True)
+        image_check_thread.start()
+        
+        # Passa os argumentos como uma tupla
+        error_check_thread = threading.Thread(target=continuously_check_errors, args=(driver, responses_file, tittle_file, name), daemon=True)
+        error_check_thread.start()
 
-        # Prompt 1
-        full_prompt = get_initial_prompt()
-        for i in range(0, len(full_prompt), 5000):
-            input_field.send_keys(full_prompt[i:i + 5000])
-            time.sleep(1)
-        input_field.send_keys(Keys.ENTER)
-        time.sleep(5)
-        # Prompt 2
-        responses_prompt = responses(responses_text)
-        for i in range(0, len(responses_prompt), 5000):
-            input_field.send_keys(responses_prompt[i:i + 5000])
-            time.sleep(1)
-        input_field.send_keys(Keys.ENTER)
-        time.sleep(15)
-        # Prompt 3
-        tittle_prompt = tittle(name)
-        for i in range(0, len(tittle_prompt), 1000):
-            input_field.send_keys(tittle_prompt[i:i + 1000])
-            time.sleep(2)
-        input_field.send_keys(Keys.ENTER)
-        time.sleep(8)
-
-        # Aguarda a resposta ser gerada e o botão de copiar estar disponível
-        while True:
-            try:
-                copy_button_location = pyautogui.locateCenterOnScreen('static/images/button_copy_chat_gpt.png')
-                if copy_button_location:
-                    pyautogui.click(copy_button_location)
-                    time.sleep(2)
-                    break
-                else:
-                    print("Botão de copiar não encontrado.")
-                    time.sleep(1)
-            except Exception as e:
-                print(f"Erro durante a automação: {e}")
-                time.sleep(1)
-        copied_tittle = pyperclip.paste()
-        with open(tittle_file, "w", encoding="utf-8") as file:
-            file.write(copied_tittle)
-        time.sleep(1)
-    
-        #
-        confirmacao = 'OK, agora me forneça o restante do conteúdo. Lembrando da hash antes de: ####Introdução, ####Sumário, ####Conteúdo e ####Conclusão. Forneça esses tópicos assim e tudo em um único texto! Apenas responda o que foi pedido, sem "essa foi a resposta, se precisar de mais..." não quero nada disso. '
-        input_field.send_keys(confirmacao)
-        time.sleep(1)
-        input_field.send_keys(Keys.ENTER)
-        time.sleep(45)
-
-        # Aguarda a resposta ser gerada e o botão de copiar estar disponível
-        while True:
-            try:
-                copy_button_location = pyautogui.locateCenterOnScreen('static/images/button_copy_chat_gpt.png')
-                if copy_button_location:
-                    pyautogui.click(copy_button_location)
-                    time.sleep(2)
-                    break
-                else:
-                    print("Botão de copiar não encontrado.")
-                    time.sleep(1)
-            except Exception as e:
-                print(f"Erro durante a automação: {e}")
-                time.sleep(1)
-        copied_text = pyperclip.paste()
-        with open(output_file, "w", encoding="utf-8") as file:
-            file.write(copied_text)
-            time.sleep(1)
+        send_prompts(driver, responses_file, tittle_file, name)
 
     finally:
-        # Encerrar a verificação contínua
         if image_check_thread is not None:
-            image_check_thread.join(timeout=10)
+            image_check_thread.join(timeout=5)
+        if error_check_thread is not None:
+            error_check_thread.join(timeout=5)
         try:
             driver.quit()
         except Exception as e:
             print(f"Erro ao encerrar o driver: {e}")
 
 if __name__ == "__main__":
-    chatgpt_response('responses.txt', 'output.txt', 'tittle.txt', 'name')
+    responses_file = 'responses.txt'
+    output_file = 'output.txt'
+    tittle_file = 'tittle.txt'
+    name = 'Victor Mello'
+    chatgpt_response(responses_file, output_file, tittle_file, name)
--- send_prompt.py
+++ send_prompt.py
@@ -1,16 +1,22 @@
 import time
 import pyautogui
 import pyperclip
+from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
 from prompt import get_initial_prompt, responses, tittle
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 
 output_file = 'output.txt'
-responses_file = 'responses.txt'
 
-def send_prompts(driver, responses_file=responses_file, tittle_file='tittle.txt', name='default_name'):
+def send_prompts(driver, responses_file, tittle_file, name):
+    # Reencontrar o campo de entrada para garantir que ele seja válido
     def get_input_field():
-        return driver.find_element(By.XPATH, '//*[@id="prompt-textarea"]')
+        return WebDriverWait(driver, 30).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="prompt-textarea"]'))
+        )
+    input_field = get_input_field()
 
     # Lê o texto do arquivo de respostas
     with open(responses_file, 'r', encoding='utf-8') as file:
@@ -18,28 +24,31 @@ def send_prompts(driver, responses_file=responses_file, tittle_file='tittle.txt'
 
     # Prompt 1
     full_prompt = get_initial_prompt()
-    input_field = get_input_field()
     for i in range(0, len(full_prompt), 5000):
         input_field.send_keys(full_prompt[i:i + 5000])
         time.sleep(1)
     input_field.send_keys(Keys.ENTER)
-    time.sleep(10)
+    time.sleep(5)
+    # Reencontrar o campo de entrada
+    input_field = get_input_field()
+    
     # Prompt 2
     responses_prompt = responses(responses_text)
-    input_field = get_input_field()
     for i in range(0, len(responses_prompt), 5000):
         input_field.send_keys(responses_prompt[i:i + 5000])
         time.sleep(1)
     input_field.send_keys(Keys.ENTER)
-    time.sleep(20)
+    time.sleep(15)
+    input_field = get_input_field()
+
     # Prompt 3
     tittle_prompt = tittle(name)
-    input_field = get_input_field()
     for i in range(0, len(tittle_prompt), 1000):
         input_field.send_keys(tittle_prompt[i:i + 1000])
         time.sleep(2)
     input_field.send_keys(Keys.ENTER)
-    time.sleep(7)
+    time.sleep(8)
+    input_field = get_input_field()
 
     # Aguarda a resposta ser gerada e o botão de copiar estar disponível e salvar num arquivo txt
     while True:
@@ -53,20 +62,19 @@ def send_prompts(driver, responses_file=responses_file, tittle_file='tittle.txt'
                 print("Botão de copiar não encontrado.")
                 time.sleep(1)
         except Exception as e:
-            print(f"Erro durante a automação de copiar: {e}")
+            print(f"Erro durante a automação: {e}")
             time.sleep(1)
     copied_tittle = pyperclip.paste()
     with open(tittle_file, "w", encoding="utf-8") as file:
         file.write(copied_tittle)
     time.sleep(1)
 
-    # Prompt 4
+    #
     confirmacao = 'OK, agora me forneça o restante do conteúdo. Lembrando da hash antes de: ####Introdução, ####Sumário, ####Conteúdo e ####Conclusão. Forneça esses tópicos assim e tudo em um único texto! Apenas responda o que foi pedido, sem "essa foi a resposta, se precisar de mais..." não quero nada disso. '
-    input_field = get_input_field()
     input_field.send_keys(confirmacao)
     time.sleep(1)
     input_field.send_keys(Keys.ENTER)
-    time.sleep(55)
+    time.sleep(60)
 
     # 
     while True:
@@ -84,10 +92,10 @@ def send_prompts(driver, responses_file=responses_file, tittle_file='tittle.txt'
                 print("imagem chatgpt nao encontrada")
                 time.sleep(1)
         except Exception as e:
-            print(f"Erro durante a automação para achar a imagem: {e}")
+            print(f"Erro para descer a página: {e}")
             time.sleep(1)
 
-    # Verifica a presença do botão 'button_copy_chat_gpt.png' e realiza ações associadas
+    # Aguarda a resposta ser gerada e o botão de copiar estar disponível e salvar num arquivo txt
     while True:
         try:
             copy_button_location = pyautogui.locateCenterOnScreen('static/images/button_copy_chat_gpt.png')
@@ -99,7 +107,7 @@ def send_prompts(driver, responses_file=responses_file, tittle_file='tittle.txt'
                 print("Botão de copiar não encontrado.")
                 time.sleep(1)
         except Exception as e:
-            print(f"Erro durante a automação de copiar a mensagem: {e}")
+            print(f"Erro durante a automação: {e}")
             time.sleep(1)
     copied_text = pyperclip.paste()
     with open(output_file, "w", encoding="utf-8") as file:
