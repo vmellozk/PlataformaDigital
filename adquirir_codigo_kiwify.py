bf16b8541c04a9c8da98d4db1a84aa9faa6b93cc
Victor <victorhugoarx@gmail.com>
2024-09-13 11:24:09 -0300

    codigo responsável por pegar o código enviado do kiwify para confirmar o login na autenticação de dois fatores

---
+++ adquirir_codigo_kiwify.py
@@ -0,0 +1,10 @@
+#
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import os
+
+#
+def adq_codigo_kw():
+    pass
