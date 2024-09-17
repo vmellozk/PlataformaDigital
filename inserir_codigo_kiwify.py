9f2c626e14b9191904ba446e8bad138589a911ab
Victor <victorhugoarx@gmail.com>
2024-09-17 11:56:57 -0300

    inserindo a verificação da barra de navegação do kiwify para continuar a automação

--- inserir_codigo_kiwify.py
+++ inserir_codigo_kiwify.py
@@ -4,20 +4,23 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
-# Verifica se o campo de verificação de dois fatores está presente para poder seguir com o código
+#
 def inserir_codigo_kw(driver):
+    # Verifica se o campo de verificação de dois fatores está presente para poder seguir com o código
     while True:
         try:
-            verificacao_dois_fatores = WebDriverWait(driver, 20).until(
-                EC.presence_of_element_located((By.XPATH, '//*[@id="__layout"]/div/div[3]/div/main/div[2]/div/div/div[1]'))
+            body = WebDriverWait(driver, 20).until(
+                EC.presence_of_element_located((By.TAG_NAME, 'body'))
             )
-            if verificacao_dois_fatores:
-                time.sleep(2)
-                print("Campo verificacao_dois_fatores encontrado")
+            
+            if "Autenticação" in body.text:
+                print("Texto 'Autenticação de 2 fatores' encontrado")
                 time.sleep(2)
                 break
+            else:
+                print("Texto 'Autenticação de 2 fatores' não encontrado")
         except Exception as e:
-            print("Aguardando o campo de 'verificacao_dois_fatores' antes de clicar...")
+            print("Aguardando o texto 'Autenticação de 2 fatores' aparecer...", e)
             time.sleep(2)
 
     # Procura onde está o campo para inserir o código, clica nele, lê o arquivo com o código e insere ele no campo
@@ -51,8 +54,22 @@ def inserir_codigo_kw(driver):
                 time.sleep(2)
                 verificar.click()
                 print("Clicando em verificar")
-                time.sleep(5)
+                time.sleep(3)
                 break
         except Exception as e:
             print("Aguardando o botão de 'verificar' antes de clicar...")
+            time.sleep(2)
+
+    # Verifica se o site carregou verificando o elemento.
+    while True:
+        try:
+            barra_nav = WebDriverWait(driver, 20).until(
+                EC.presence_of_element_located((By.XPATH, '//*[@id="__layout"]/div/div[1]/div[5]/div[2]/div/div/div[2]/nav/div[1]'))
+            )
+            if barra_nav:
+                print("Campo barra_nav encontrado")
+                time.sleep(2)
+                break
+        except Exception as e:
+            print("Aguardando o campo de 'barra_nav' antes de clicar...")
             time.sleep(2)
